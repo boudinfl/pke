db1d21c1bd918b2c0d8c1a0f33f8ac0e71d50e72
Florian Boudin <florian.boudin@univ-nantes.fr>
2016-10-04 17:06:54 +0200

    re-establish feature scaling

--- pke/supervised.py
+++ pke/supervised.py
@@ -138,8 +138,8 @@ class Kea(SupervisedLoadFile):
             self.instances[k] = np.array([len(v.surface_forms) * idf,
                                           v.offsets[0]/maximum_offset])
 
-        # # scale features
-        # self.feature_scaling()
+        # scale features
+        self.feature_scaling()
 
 
     @staticmethod
@@ -351,8 +351,8 @@ class WINGNUS(SupervisedLoadFile):
             self.instances[k] = np.array([feature_array[i-1] for i \
                                           in features_set])
 
-        # # scale features
-        # self.feature_scaling()
+        # scale features
+        self.feature_scaling()
 
 
     @staticmethod
