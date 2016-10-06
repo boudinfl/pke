a95d91a7c4e70350e6c71ee4b28a60dd7327f26c
Florian Boudin <florian.boudin@univ-nantes.fr>
2016-10-06 13:07:39 +0200

    update 1.04 and adding MANIFEST.in for models

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='pke',
-      version='1.03',
+      version='1.04',
       description='Python Keyphrase Extraction module',
       author='Florian Boudin',
       author_email='florian.boudin@univ-nantes.fr',
+      license='gnu',
       packages=['pke'],
       url="https://github.com/boudinfl/pke",
       install_requires=[
