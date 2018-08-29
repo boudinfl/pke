d0ad8305fde04035dc7b84d60955f8bf1f9ed219
Florian Boudin <florian.boudin@univ-nantes.fr>
2018-08-29 10:13:09 +0200

    Adding an example of training/testing procedure for Kea

---
+++ examples/training_and_testing_a_kea_model/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pke
+
+# create a Kea extractor and set the input language to English (used for
+# the stoplist in the candidate selection method)
+extractor = pke.supervised.Kea(input_file='C-1.xml',
+                			   language='english')
+
+# load the content of the document, here in CoreNLP XML format
+# the use_lemmas parameter allows to choose using CoreNLP lemmas or stems 
+# computed using nltk
+extractor.read_document(format='corenlp',
+                        use_lemmas=False)
+
+# select the keyphrase candidates, for Kea the 1-3 grams that do not start or
+# end with a stopword.
+extractor.candidate_selection()
+
+# load the df counts
+df_counts = pke.load_document_frequency_file(input_file="df.tsv.gz",
+											  delimiter='\t')
+
+# weight the candidates using Kea model.
+extractor.candidate_weighting(model_file="model.pickle", df=df_counts)
+
+# print the n-highest (10) scored candidates
+for (keyphrase, score) in extractor.get_n_best(n=10):
+	print(keyphrase, score)
