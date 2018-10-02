5d34e26bab97ee5fc974fc286dd505545edde6f5
Florian Boudin <florian.boudin@univ-nantes.fr>
2018-10-02 14:03:41 +0200

    Adding Skeletton model for ExpandRank

--- pke/unsupervised/__init__.py
+++ pke/unsupervised/__init__.py
@@ -8,6 +8,7 @@ from pke.unsupervised.graph_based.singlerank import *
 from pke.unsupervised.graph_based.multipartiterank import *
 from pke.unsupervised.graph_based.positionrank import *
 from pke.unsupervised.graph_based.single_tpr import *
+from pke.unsupervised.graph_based.expandrank import *
 
 from pke.unsupervised.statistical.tfidf import *
 from pke.unsupervised.statistical.kpminer import *
