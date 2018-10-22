52b66c394ab747783ea23e8102d7548581c16d31
Ygor Gallina <ygor.gallina@gmail.com>
2018-10-22 08:32:33 +0200

    Cleaned namespaces

--- pke/__init__.py
+++ pke/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
-from pke.base import *
-from pke.utils import *
-from pke.readers import *
+from pke.base import Sentence, Candidate, LoadFile
+from pke.utils import (load_document_frequency_file, compute_document_frequency,
+                       train_supervised_model, load_references, compute_lda_model,
+                       load_document_as_bos, compute_pairwise_similarity_matrix)
+from pke.readers import MinimalCoreNLPParser, PreProcessedTextReader, RawTextReader
 import pke.unsupervised
 import pke.supervised
