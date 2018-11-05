a673cd54955d62e03057df2becd3dd530fe11f05
Florian Boudin <florian.boudin@univ-nantes.fr>
2018-11-05 17:27:56 +0100

    Update tests/test_textrank.py

--- tests/test_textrank.py
+++ tests/test_textrank.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 import pke
 
-valid_pos = {'NOUN', 'PROPN', 'ADJ'}
-test_file = 'examples/C-1.xml'
+text = u"Compatibility of systems of linear constraints over the set of natural\
+ numbers. Criteria of compatibility of a system of linear Diophantine equations\
+, strict inequations, and nonstrict inequations are considered. Upper bounds fo\
+r components of a minimal set of solutions and algorithms of construction of mi\
+nimal generating sets of solutions for all types of systems are given. These cr\
+iteria and the corresponding algorithms for constructing a minimal supporting s\
+et of solutions can be used in solving all the considered types systems and sys\
+tems of mixed types."
 
+pos = {'NOUN', 'PROPN', 'ADJ'}
 
-def test_textrank_candidate_selection():
+def test_textrank():
+    """Test TextRank for keyword extraction using original paper's example."""
+
     extractor = pke.unsupervised.TextRank()
-    extractor.load_document(test_file)
-
-    extractor.candidate_selection(pos=valid_pos)
-
-    assert len(extractor.candidates) == 567
-
-
-def test_textrank_candidate_weighting():
-    extractor = pke.unsupervised.TextRank()
-    extractor.load_document(test_file)
-
-    extractor.candidate_selection(pos=valid_pos)
-
-    extractor.candidate_weighting()
-
+    extractor.load_document(input=text)
+    extractor.candidate_weighting(T=.33, pos=pos)
     keyphrases = [k for k, s in extractor.get_n_best(n=3)]
+    assert keyphrases == ['linear diophantine',
+                          'natural numbers',
+                          'types']
 
-    assert keyphrases == ['dht based uddi registry hierarchies',
-                          'multiple proxy uddi registries',
-                          'new local uddi registry figure']
+def test_textrank_with_candidate_selection():
+    """Test TextRank with longest-POS-sequences candidate selection."""
+
+    extractor = pke.unsupervised.TextRank()
+    extractor.load_document(input=text)
+    extractor.candidate_selection(pos=pos)
+    extractor.candidate_weighting(pos=pos)
+    keyphrases = [k for k, s in extractor.get_n_best(n=3)]
+    assert keyphrases == ['linear diophantine equations',
+                          'minimal generating sets',
+                          'mixed types']
 
 
 if __name__ == '__main__':
-    test_textrank_candidate_selection()
-    test_textrank_candidate_weighting()
+    test_textrank()
+    test_textrank_with_candidate_selection()
