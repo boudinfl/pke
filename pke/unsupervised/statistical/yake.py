6c049bd47ad29654b5b013b11dd9ff4aa9dcc395
Florian Boudin <florian.boudin@univ-nantes.fr>
2019-09-25 09:51:59 +0900

    updating YAKE reference

--- pke/unsupervised/statistical/yake.py
+++ pke/unsupervised/statistical/yake.py
@@ -8,8 +8,8 @@ Statistical approach to keyphrase extraction described in:
 
 * Ricardo Campos, Vítor Mangaravite, Arian Pasquali, Alípio Mário Jorge,
   Célia Nunes and Adam Jatowt.
-  YAKE! Collection-Independent Automatic Keyword Extractor.
-  *Proceedings of ECIR*, pages 806-810, 2018.
+  YAKE! Keyword extraction from single documents using multiple local features.
+  *Information Sciences*, pages 257-289, 2020.
 
 """
 
