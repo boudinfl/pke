160a0aaeed30e5cf012581d5cf855259dcc7eb9f
Poulain <timothee.poulain@gmail.com>
2020-04-11 10:46:24 +0200

    add test_expandrank

--- tests/test_expandrank.py
+++ tests/test_expandrank.py
@@ -4,8 +4,11 @@
 from __future__ import unicode_literals
 
 import pke
+from collections import defaultdict
+import gzip
+import bisect
 
-input_file = 'data_cluster/FT923-5089.xml'
+input_file = 'data/FT923-5089.xml'
 pos = {'NOUN', 'PROPN', 'ADJ'}
 
 
@@ -37,14 +40,33 @@ def test_singlerank_candidate_weighting():
     keyphrases = [k for k, s in extractor.get_n_best(n=10)]
     print(keyphrases)
 
+def load_pairwise_similarities(path):
+    """Load the pairwise similarities for ExpandRank."""
+
+    pairwise_sim = defaultdict(list)
+    with gzip.open(path, 'rb') as f:
+        lines = f.readlines()
+        for line in lines:
+            cols = line.decode('utf-8').strip().split()
+            bisect.insort(pairwise_sim[cols[0].split("/")[-1]], (float(cols[2]), cols[1].split("/")[-1]))
+            bisect.insort(pairwise_sim[cols[1].split("/")[-1]], (float(cols[2]), cols[0].split("/")[-1]))
+    return pairwise_sim
+
 
 if __name__ == '__main__':
-    test_expandrank_candidate_selection()
-    expanded_docs = []
-    with open("data_cluster/test_cluster.txt", "r") as file:
-        line = file.readline().split(",")
-        if(line[0] == input_file):
-            expanded_docs = line[1]
-    test_expandrank_candidate_weighting(expanded_docs)
+    """ You will first need to perform the function compute_document_frequency(input_dir=input_dir, ...)
+    and compute the pairwise similarity measures compute_pairwise_similarity_matrix(input_dir=input_dir,...)
+    in order to have the parwise similarity measures file.
 
+    python3 compute-df-counts.py path/to/folderfile df_count.gz
+    python3 compute-pairwise-similarity-matrix.py path/to/folderfile output.gz None df_count.gz
+
+    """
+
+    # hyperparameters neighbor numbers
+    n_expanded = 1
+    pairwise = load_pairwise_similarities("data/Output.gz")
+    test_expandrank_candidate_selection()
+    expanded_documents = [(v, u) for u, v in pairwise[input_file.split("/")[-1]][-n_expanded:]]
+    test_expandrank_candidate_weighting(expanded_documents)
     test_singlerank_candidate_weighting()
