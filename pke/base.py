3714366c44ad92d85f786a0deedd7db14a809129
Ygor Gallina <ygor.gallina@gmail.com>
2020-09-24 10:20:32 +0200

    Removing warning in get_n_best

--- pke/base.py
+++ pke/base.py
@@ -298,11 +298,6 @@ class LoadFile(object):
             n_best = [(' '.join(self.candidates[u].surface_forms[0]).lower(),
                        self.weights[u]) for u in best[:min(n, len(best))]]
 
-        if len(n_best) < n:
-            logging.warning(
-                'Not enough candidates to choose from '
-                '({} requested, {} given)'.format(n, len(n_best)))
-
         # return the list of best candidates
         return n_best
 
